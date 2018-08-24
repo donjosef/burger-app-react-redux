720a7c536ab61d0385b8b865d1fc742c92651960
donjoe <giumontanaro@gmail.com>
2018-08-24 19:01:43 +0200

    use Logo in the toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo'
 
 const toolbar = props => (
-    <header className={classes.Toolbar}>Navigation...</header>
+    <header className={classes.Toolbar}>
+        <div>Menu Button</div>
+        <Logo />
+    </header>
 )
 
 export default toolbar;
