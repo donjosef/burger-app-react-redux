4573081db1fe610f09989f7280f3c22aaeea7981
donjoe <giumontanaro@gmail.com>
2018-08-24 17:57:38 +0200

    created  toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './Toolbar.css'
+
+const toolbar = props => (
+    <header className={classes.Toolbar}>Navigation...</header>
+)
+
+export default toolbar;
