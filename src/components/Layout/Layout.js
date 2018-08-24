efe176859a3c43542f75411a190cb82474a60ef3
donjoe <giumontanaro@gmail.com>
2018-08-24 22:27:39 +0200

    use sidebar inside layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Aux from '../../hoc/Auxiliary'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
-
+import SideBar from '../Navigation/SideBar/SideBar'
 const layout = props => {
     return (
         <Aux>
             <Toolbar />
+            <SideBar />
             <main className={classes.content}>
                 {props.children}
             </main>
