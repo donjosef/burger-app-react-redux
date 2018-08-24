60259378f56515a00882c746e2b01a45945c139f
donjoe <giumontanaro@gmail.com>
2018-08-24 17:58:34 +0200

    use toolbar component in the layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Auxiliary'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
 
 const layout = props => {
     return (
         <Aux>
-            <div>ToolBar, SideBar, BackDrop</div>
+            <Toolbar />
             <main className={classes.content}>
                 {props.children}
             </main>
