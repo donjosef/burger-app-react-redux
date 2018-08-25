4f97e8e5d5d035811e9bef16b9e1c2203a99730b
donjoe <giumontanaro@gmail.com>
2018-08-25 15:45:13 +0200

    checking proptypes in orderSummary

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliary'
 import Button from '../Button/Button'
+import PropTypes from 'prop-types'
 
-const orderSummary = props => {
-    const {hideModal, continuePurchase} = props;
-    const summaryIngredients = Object.keys(props.ingredients).map(igKey => (
-        <li key={igKey}>
-            <span style={{textTransform: "uppercase"}}>{igKey}: {props.ingredients[igKey]}</span>
-        </li>
-    ));
-    
-    return (
-        <Aux>
-            <h3>Order summary:</h3>
-            <hr />
-            <p>You burger has the following ingredients:</p>
-            <ul>
-               {summaryIngredients}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button clicked={hideModal} type="Danger">CANCEL</Button>
-            <Button clicked={continuePurchase} type="Success">CONTINUE</Button>
-        </Aux>
-    );
-    
+class OrderSummary extends Component { 
+    render() {
+         const {ingredients, price, hideModal, continuePurchase} = this.props;
+         const summaryIngredients = Object.keys(ingredients).map(igKey => (
+                <li key={igKey}>
+                    <span style={{textTransform: "uppercase"}}>{igKey}: {ingredients[igKey]}</span>
+                </li>
+        ));
+        
+        return (
+            <Aux>
+                <h3>Order summary:</h3>
+                <hr />
+                <p>You burger has the following ingredients:</p>
+                <ul>
+                   {summaryIngredients}
+                </ul>
+                <p><strong>Total Price: ${price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button clicked={hideModal} type="Danger">CANCEL</Button>
+                <Button clicked={continuePurchase} type="Success">CONTINUE</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary;
+OrderSummary.propTypes = {
+    ingredients: PropTypes.object.isRequired,
+    hideModal: PropTypes.func.isRequired,
+    continuePurchase: PropTypes.func.isRequired,
+    price: PropTypes.number.isRequired
+};
+
+export default OrderSummary;
