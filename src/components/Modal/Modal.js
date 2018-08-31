3c0e9bf35ab7edb904e5f1e97a6651340213bb41
donjoe <giumontanaro@gmail.com>
2018-08-31 10:33:57 +0200

    finetune shouldCOmponentUpdate to make sure it renders properly

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,11 +4,12 @@ import classes from './Modal.css'
 class Modal extends Component {
     
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show;
-    } //improve performance. So for example if i click on more or less buttons, avoid to update the modal
+        return nextProps.show !== this.props.show || (this.props.show && nextProps.children !== this.props.children);
+    } //improve performance. So for example if i click on more or less buttons, avoid to update the modal. The second check is important for the spinner to display
     
     
     render() {
+        console.log("[updating modal...]")
         let modalClasses = [];
         if(this.props.show) {
             modalClasses.push(classes.Show);
