0ed48b991bd859a673d99810ef75ceb81f33281c
donjoe <giumontanaro@gmail.com>
2018-09-05 10:56:13 +0200

    render different routes with route component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+        <BrowserRouter>
+            <div>
+              <Layout>
+                <Route exact path='/' component={BurgerBuilder}/>
+                <Route path='/my-orders' component={Checkout}/>
+              </Layout>
+            </div>
+        </BrowserRouter>
     );
   }
 }
