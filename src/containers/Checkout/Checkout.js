602cf4c853218c3b3b4ebafa43c573903de3b692
donjoe <giumontanaro@gmail.com>
2018-09-05 18:29:52 +0200

    extract queryparams from url to use them as ingredients in checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,11 +2,35 @@ import React, {Component} from 'react';
 import CheckoutSummary from '../../components/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
+    state = {
+        ingredients: {}
+    }
+    
+    componentDidMount() {
+        const params = new URLSearchParams(this.props.location.search); //iterator. Extract the query params from url
+        const ingredients = {};
+        for(let param of params) {
+          ingredients[param[0]] = Number(param[1]);
+        }
+
+        this.setState({ ingredients });
+    }
+    
+    backToHomeHandler = () => {
+        this.props.history.push('/');
+    }
+    
+    continueCheckoutHandler = () => {
+        this.props.history.push('/my-orders/contact-data');
+    }
     
     render() {
         return (
             <div>
-              <CheckoutSummary />
+              <CheckoutSummary 
+                ingredients={this.state.ingredients}
+                backToHomeHandler={this.backToHomeHandler}
+                continueCheckout={this.continueCheckoutHandler}/>
             </div>
         );
     }
