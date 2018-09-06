ad5d17d66a581590c99ca516b0e4441962f6a352
donjoe <giumontanaro@gmail.com>
2018-09-06 16:41:42 +0200

    import NavLink to use inside navigationItem component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import classes from './NavigationItem.css'
+import { NavLink } from 'react-router-dom'
 
+/* activeClassName={classes.active} viene utilizzata per via dei css modules che stiamo usando. Se usassimo semplicemente la classe active fornitaci automaticamente da NavLink, dato che usiamo i css modules come classes.something, le classi vengono fuori con un nome diverso da active, quindi nessuna classe active verrebbe applicata*/
 const navigationItem = props => (
     <li className={classes.NavigationItem}>
-        <a 
-            href={props.link} 
-            className={props.active ? classes.Active : null}>{props.children}</a>
+        <NavLink exact to={props.link} activeClassName={classes.active}>{props.children}</NavLink>
     </li>
 );
 
