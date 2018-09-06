c5f812027d9c22b53c83ca22af3d37e240493240
donjoe <giumontanaro@gmail.com>
2018-09-06 16:38:25 +0200

    render orders component to path /orders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
+import Orders from './containers/Orders/Orders'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import {BrowserRouter, Route} from 'react-router-dom'
@@ -12,6 +13,7 @@ class App extends Component {
             <div>
               <Layout>
                 <Route exact path='/' component={BurgerBuilder}/>
+                <Route path='/orders' component={Orders}/>
                 <Route path='/checkout' component={Checkout}/>
               </Layout>
             </div>
