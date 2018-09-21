4a5451d2b045cd7821a7f76fd290c85484df6627
donjoe <giumontanaro@gmail.com>
2018-09-21 18:48:39 +0200

    use redux-thunk to handle async code

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import { Provider } from 'react-redux';
 import ingrReducer from './store/reducers/ingredients';
 import totalPriceReducer from './store/reducers/totalPrice';
+import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
   ingredients: ingrReducer,
   totalPrice: totalPriceReducer
 });
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
