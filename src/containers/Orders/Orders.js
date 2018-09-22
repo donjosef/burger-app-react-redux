e22a4b7aa678431680a1caa5a132e163e9396b1b
donjoe <giumontanaro@gmail.com>
2018-09-22 23:50:35 +0200

    When component mounts dispatch an async request

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import { fetchOrders } from '../../store/actions/orders';
+import { connect } from 'react-redux';
 
 class Orders extends Component {
-    
+
+  componentDidMount() {
+    this.props.fetchOrders() //now handled by redux thunk and action creators
+  }
     render() {
+
         return (
             <div>
                 <Order />
@@ -13,4 +19,17 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+const mapStateToProps = state => {
+  return {
+    orders: state.orders.orders,
+    loading: state.orders.loading,
+    err: state.orders.ordersErr
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchOrders: () => dispatch(fetchOrders())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
