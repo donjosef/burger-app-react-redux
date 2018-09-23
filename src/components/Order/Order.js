f1d7f019feab10c34f567e98a5d867d795a5b1da
donjoe <giumontanaro@gmail.com>
2018-09-23 12:06:30 +0200

    Transform ingredients in array of span elements to render ingredients inside Order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = props => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>$ 4.5</strong></p>
-    </div>
-);
+const order = props => {
+  const orderIngredients = Object.keys(props.ingredients).map(igKey => (
+         <span key={igKey}>
+             {igKey}({props.ingredients[igKey]})
+         </span>
+  )).reduce((prev, curr) => [prev, ' ', curr]); //transform ingredients object in array of span element, then separate them with a blank space
+
+    return (
+      <div className={classes.Order}>
+        <p>Ingredients: {orderIngredients}</p>
+        <p>Price: <strong>{props.price}</strong></p>
+      </div>
+
+    )
+
+
+}
 
 export default order;
