8b94d1300d9f83d9960dadebb761290d26384bf7
donjoe <giumontanaro@gmail.com>
2018-09-24 23:01:12 +0200

    Combine auth reducer in root reducer

--- src/index.js
+++ src/index.js
@@ -9,13 +9,15 @@ import { Provider } from 'react-redux';
 import ingrReducer from './store/reducers/ingredients';
 import totalPriceReducer from './store/reducers/totalPrice';
 import ordersReducer from './store/reducers/orders'
+import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
   ingredients: ingrReducer,
   totalPrice: totalPriceReducer,
-  orders: ordersReducer
+  orders: ordersReducer,
+  auth: authReducer
 });
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
