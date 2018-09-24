4b08c21500367f172010d6a181af04f8859987c2
donjoe <giumontanaro@gmail.com>
2018-09-24 17:29:18 +0200

    Define action types for authentication

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,7 +1,12 @@
 export const INCREASE = 'INCREASE';
 export const DECREASE = 'DECREASE';
+
 export const SET_INGREDIENTS = 'SET_INGREDIENTS';
 export const FETCH_ORDERS_INIT = 'FETCH_ORDERS_INIT';
 export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
 export const FETCH_ORDERS_FAIL = 'FETCH_ORDERS_FAIL';
 export const RESET = 'RESET';
+
+export const AUTH_INIT = 'AUTH_INIT';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
