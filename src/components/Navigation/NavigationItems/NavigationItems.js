edb8ddde6b9ee6052faaceb328320d3eb819205f
donjoe <giumontanaro@gmail.com>
2018-09-24 15:21:34 +0200

    Create a route Link for auth component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,20 +4,21 @@ import classes from './NavigationItems.css';
 
 const navigationItems = props => {
     let dynamicClasses = [];
-    
+
     if(props.type === "Mobile") {
-        dynamicClasses.push(classes.Mobile) 
+        dynamicClasses.push(classes.Mobile)
     } else {
         dynamicClasses.push(classes.Desktop)
     }
-    
+
     return (
         <ul className={dynamicClasses.join(" ")}>
             <NavigationItem link="/">Burger Builder</NavigationItem>
             <NavigationItem link="/orders">Orders</NavigationItem>
+            <NavigationItem link="/auth">Authenticate</NavigationItem>
         </ul>
     )
-    
+
 }
 
 export default navigationItems;
