ac3d274654968e190880761fac4f072adc48a685
donjoe <giumontanaro@gmail.com>
2018-09-24 15:22:25 +0200

    Render Auth component when the path matches the Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import {BrowserRouter, Route} from 'react-router-dom'
@@ -14,6 +15,7 @@ class App extends Component {
               <Layout>
                 <Route exact path='/' component={BurgerBuilder}/>
                 <Route path='/orders' component={Orders}/>
+                <Route path='/auth' component={Auth} />
                 <Route path='/checkout' component={Checkout}/>
               </Layout>
             </div>
