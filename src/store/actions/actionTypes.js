17f1a20694981734951ab5c02bed2604f28bdcad
donjoe <giumontanaro@gmail.com>
2018-09-25 11:20:47 +0200

    Define actiontypes for logout

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -10,3 +10,4 @@ export const RESET = 'RESET';
 export const AUTH_INIT = 'AUTH_INIT';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAIL = 'AUTH_FAIL';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
